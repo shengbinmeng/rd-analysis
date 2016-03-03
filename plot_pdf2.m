11edab243bb67e76e47fbfa0f95d45c1e91d93d4
Shengbin Meng <shengbinmeng@gmail.com>
2016-03-03 20:16:50 +0800

    Set some figure properties and change some distribution parameters

--- plot_pdf2.m
+++ plot_pdf2.m
@@ -9,17 +9,28 @@ offset = 0.5;
 
 figure;
 hold all;
-plot(x - 2*dis, stblpdf(x, 0.75, beta, gam, delta, 'quick'), 'r');
-plot(x - dis, stblpdf(x, 1, beta, gam, delta, 'quick'), 'r'); % Cauchy
-plot(x, stblpdf(x, 1.5, beta, gam, delta, 'quick'), 'r');
-plot(x + dis, stblpdf(x, 2, beta, 1/(2^0.5), delta, 'quick'), 'r'); % Gaussian
+lw = 1.5;
+plot(x - 2*dis, stblpdf(x, 0.75, beta, gam, delta, 'quick'), 'k', 'LineWidth',lw);
+plot(x - dis, stblpdf(x, 1, beta, gam, delta, 'quick'), 'r', 'LineWidth',lw); % Cauchy
+plot(x, stblpdf(x, 1.5, beta, gam, delta, 'quick'), 'k', 'LineWidth',lw);
+plot(x + dis, stblpdf(x, 2, beta, 1/(2^0.5), delta, 'quick'), 'r', 'LineWidth',lw); % Gaussian
 
-plot(x - dis, gcd(x, 1.5, s) - offset, 'g');
-plot(x - dis, gcd(x, 2, s), 'r'); % Cauchy
-plot(x - dis, gcd(x, 2.5, s) + offset, 'b');
-plot(x - dis, gcd(x, 3.5, s) + 2*offset, 'c');
+plot(x - dis, gcd(x, 1.5, s) - offset, 'k', 'LineWidth',lw);
+plot(x - dis, gcd(x, 2, s), 'r', 'LineWidth',lw); % Cauchy
+plot(x - dis, gcd(x, 3, s) + offset, 'k', 'LineWidth',lw);
+plot(x - dis, gcd(x, 8, s) + 2*offset, 'k', 'LineWidth',lw);
 
-plot(x + dis, ggd(x, 1.5, s) - offset, 'g');
+plot(x + dis, ggd(x, 1.5, s) - offset, 'k', 'LineWidth',lw);
 plot(x + dis, ggd(x, 2, s), 'r'); % Gaussian
-plot(x + dis, ggd(x, 2.5, s) + offset, 'b');
-plot(x + dis, ggd(x, 3.5, s) + 2*offset, 'c');
+plot(x + dis, ggd(x, 3, s) + offset, 'k', 'LineWidth',lw);
+plot(x + dis, ggd(x, 8, s) + 2*offset, 'k', 'LineWidth',lw);
+
+ax = gca;
+set(ax,'XTick',[-2*dis, -dis, 0, dis]);
+set(ax,'XTickLabel',{'0.75','1','1.5','2'});
+XLabel('Stability Parameter','fontsize',12);
+set(ax,'YTick',[-offset+0.2 0+0.2 offset+0.15 2*offset+0.15]);
+set(ax,'YTickLabel',{'1.5','2','3','8'});
+YLabel('Shaple Paremeter','fontsize',12);
+set(ax,'LineWidth', lw);
+set(ax,'FontSize', 12);
